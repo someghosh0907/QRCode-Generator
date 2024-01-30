8af2335fd2534bc4296cd3ebe836b2e0f0bd7eb0
Soumyajit Ghosh <113449899+someghosh0907@users.noreply.github.com>
2024-01-30 18:35:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import QRCode from "react-qr-code";
+import useLocalStorage from "./LightDarkMode/useLocalStorage";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [qrcode, setQrcode] = useState("");
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
+  //const [displayTheme, setDisplayTheme] = useState("dark");
+
+  function handleToggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  const handleGenerate = () => {
+    setQrcode(input);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="light-dark-mode" data-theme={theme}>
+        <div className="container">
+          <div className="header">
+            <h1>QR Code Generator</h1>
+            {theme === "dark" ? (
+              <FaSun
+                style={{ color: "yellow" }}
+                onClick={handleToggleTheme}
+                className="theme-type"
+              ></FaSun>
+            ) : (
+              <FaMoon onClick={handleToggleTheme} className="theme-type"></FaMoon>
+            )}
+          </div>
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            name="qr-code"
+            placeholder="Enter Your Value Here"
+          />
+          <button
+            disabled={input && input.trim() !== "" ? false : true}
+            onClick={handleGenerate}
+          >
+            Generate
+          </button>
+          <br />
+          <div className="qrcode">
+            <QRCode
+              id="qr-code-value"
+              value={qrcode}
+              size={380}
+              bg="darkblue"
+            />
+          </div>
+        </div>
+      </div>
+      <div>Made By Soumyajit Ghosh</div>
     </div>
   );
 }
